4c704569dc18f382c68a3eb58fa934e2696adbb8
Igor01 <Ih01@i.ua>
2016-02-12 22:11:59 +0200

    some faults

--- walls.cs
+++ walls.cs
@@ -11,21 +11,21 @@ namespace consoleSnake
         List<figure> wallList;
         public portal Portal;
 
-        public walls(int whidth, int height, int lvl)
+        public walls(int width, int height, int lvl)
         {
             wallList = new List<figure>();
-            line_horizontal wall1 = new line_horizontal(0, whidth - 1, 0, 1);
-            line_horizontal wall2 = new line_horizontal(0, whidth - 1, height - 1, 1);
+            line_horizontal wall1 = new line_horizontal(0, width - 1, 0, 1);
+            line_horizontal wall2 = new line_horizontal(0, width - 1, height - 1, 1);
             line_vertical wall3 = new line_vertical(0, height - 1, 0, 1);
-            line_vertical wall4 = new line_vertical(0, height - 1, whidth - 1, 1);
+            line_vertical wall4 = new line_vertical(0, height - 1, width - 1, 1);
             wallList.Add(wall1);
             wallList.Add(wall2);
             wallList.Add(wall3);
             wallList.Add(wall4);
             if (lvl == 2)
             {
-                line_horizontal wall5 = new line_horizontal(3, whidth - 4, height / 2, 1);
-                line_vertical wall6 = new line_vertical(3, height - 4, whidth / 2, 1);
+                line_horizontal wall5 = new line_horizontal(3, width - 4, height / 2, 1);
+                line_vertical wall6 = new line_vertical(3, height - 4, width / 2, 1);
                 wallList.Add(wall5);
                 wallList.Add(wall6);
             }
@@ -33,8 +33,8 @@ namespace consoleSnake
             {
                 line_vertical wall5 = new line_vertical(1, height / 2 - 1, 10, 1);
                 line_vertical wall6 = new line_vertical(height / 2 + 3, height - 1, 10, 1);
-                line_vertical wall7 = new line_vertical(1, height / 2 - 1, whidth - 11, 1);
-                line_vertical wall8 = new line_vertical(height / 2 + 3, height - 1, whidth - 11, 1);
+                line_vertical wall7 = new line_vertical(1, height / 2 - 1, width - 11, 1);
+                line_vertical wall8 = new line_vertical(height / 2 + 3, height - 1, width - 11, 1);
 
                 wallList.Add(wall5);
                 wallList.Add(wall6);
@@ -43,16 +43,16 @@ namespace consoleSnake
 
                 line_horizontal wall9;
                 if (lvl == 3)
-                    wall9 = new line_horizontal(12, whidth - 13, height / 2 + 1, 1);
+                    wall9 = new line_horizontal(12, width - 13, height / 2 + 1, 1);
                 else
-                    wall9 = new line_horizontal(11, whidth - 12, height / 2 + 1, 1);
+                    wall9 = new line_horizontal(11, width - 12, height / 2 + 1, 1);
                 wallList.Add(wall9);
                 if (lvl == 4)
                 {
                     line_vertical wall10 = new line_vertical(height / 2 - 2, height / 2 + 4, 7, 1);
-                    line_vertical wall11 = new line_vertical(height / 2 - 2, height / 2 + 4, whidth - 8, 1);
-                    line_horizontal wall12 = new line_horizontal(whidth / 2 - 5, whidth / 2 + 5, 5, 1);
-                    line_horizontal wall13 = new line_horizontal(whidth / 2 - 5, whidth / 2 + 5, height - 6, 1);
+                    line_vertical wall11 = new line_vertical(height / 2 - 2, height / 2 + 4, width - 8, 1);
+                    line_horizontal wall12 = new line_horizontal(width / 2 - 5, width / 2 + 5, 5, 1);
+                    line_horizontal wall13 = new line_horizontal(width / 2 - 5, width / 2 + 5, height - 6, 1);
                     wallList.Add(wall10);
                     wallList.Add(wall11);
                     wallList.Add(wall12);
@@ -62,12 +62,12 @@ namespace consoleSnake
 
             if (lvl == 5)
             {
-                line_horizontal wall5 = new line_horizontal(2, whidth - 3, height / 2, 1);
-                line_vertical wall6 = new line_vertical(2, height - 3, whidth / 2, 1);
-                line_vertical wall7 = new line_vertical(1, height / 2 - 2, whidth / 4, 1);
-                line_vertical wall8 = new line_vertical(height / 2 + 2, height - 2, whidth / 4, 1);
-                line_vertical wall9 = new line_vertical(1, height / 2 - 2, whidth / 4 * 3, 1);
-                line_vertical wall10 = new line_vertical(height / 2 + 2, height - 2, whidth / 4 * 3, 1);
+                line_horizontal wall5 = new line_horizontal(2, width - 3, height / 2, 1);
+                line_vertical wall6 = new line_vertical(2, height - 3, width / 2, 1);
+                line_vertical wall7 = new line_vertical(1, height / 2 - 2, width / 4, 1);
+                line_vertical wall8 = new line_vertical(height / 2 + 2, height - 2, width / 4, 1);
+                line_vertical wall9 = new line_vertical(1, height / 2 - 2, width / 4 * 3, 1);
+                line_vertical wall10 = new line_vertical(height / 2 + 2, height - 2, width / 4 * 3, 1);
                 wallList.Add(wall5);
                 wallList.Add(wall6);
                 wallList.Add(wall7);
@@ -78,20 +78,20 @@ namespace consoleSnake
 
             if (lvl == 6 || lvl == 7)
             {
-                line_vertical wall5 = new line_vertical(1, height - 2, whidth / 2, 1);
+                line_vertical wall5 = new line_vertical(1, height - 2, width / 2, 1);
                 wallList.Add(wall5);
-                Portal = new portal(5, height / 2, whidth - 6, height / 2);
+                Portal = new portal(5, height / 2, width - 6, height / 2);
             }
 
             if (lvl == 7)
             {
                 line_vertical wall6 = new line_vertical(1, height / 2 - 1, 10, 1);
                 line_vertical wall7 = new line_vertical(height / 2 + 3, height - 1, 10, 1);
-                line_vertical wall8 = new line_vertical(1, height / 2 - 1, whidth - 11, 1);
-                line_vertical wall9 = new line_vertical(height / 2 + 3, height - 1, whidth - 11, 1);
-                line_horizontal wall12 = new line_horizontal(12, whidth - 13, height / 2 + 1, 1);
+                line_vertical wall8 = new line_vertical(1, height / 2 - 1, width - 11, 1);
+                line_vertical wall9 = new line_vertical(height / 2 + 3, height - 1, width - 11, 1);
+                line_horizontal wall12 = new line_horizontal(12, width - 13, height / 2 + 1, 1);
                 line_vertical wall10 = new line_vertical(height / 2 - 2, height / 2 + 4, 7, 1);
-                line_vertical wall11 = new line_vertical(height / 2 - 2, height / 2 + 4, whidth - 8, 1);
+                line_vertical wall11 = new line_vertical(height / 2 - 2, height / 2 + 4, width - 8, 1);
 
                 wallList.Add(wall9);
                 wallList.Add(wall6);
